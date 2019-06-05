fc9e25e9c86fd14d272770e38e7fc9e13a96c266
你好呀Panda <Irenewsh@Shiheng.fios-router.home>
2019-06-04 21:28:14 -0400

    Comments add in code

--- dogClass.java
+++ dogClass.java
@@ -1,10 +1,12 @@
 public class dogClass {
+	// create the weight variable 
 	public int weight_of_it;
 
+	/** this is just like __init__(self, w) in Python */
 	public dogClass(int w){
 		weight_of_it = w;
 	}
-
+	// create the makeNoise function
 	public void makeNoise() {
 		if (weight_of_it < 10){
 			System.out.println("I am a poppy.");
